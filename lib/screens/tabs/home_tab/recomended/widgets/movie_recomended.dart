d3d1394b46034d91fc7dd21c3869c75306dee9ee
Osama mohamed <om329211@gmail.com>
2024-12-13 13:57:38 +0200

    +recommended ui

--- lib/screens/tabs/home_tab/recomended/widgets/movie_recomended.dart
+++ lib/screens/tabs/home_tab/recomended/widgets/movie_recomended.dart
@@ -11,7 +11,7 @@ class MovieRecomended extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding:  EdgeInsets.all(8.0),
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15),
